534b26d2abded98f1de58c29d54b3c0ca2f82216
Eugenio Perez <eupm90@gmail.com>
2013-12-10 16:29:25 +0000

    Added groups and vector operation tests

--- test.sh
+++ test.sh
@@ -2,22 +2,23 @@ PROGNAME=$1
 CONFIG_FILE=$2
 
 PREPROC_RESULT=${CONFIG_FILE}_result.tmp
+PREPROC_ERROR=${CONFIG_FILE}_error.tmp
 POSTPROC_RESULT=${CONFIG_FILE}_result.txt
 EXPECTED_RESULT=${CONFIG_FILE}.result
 TMP_DIFF=${CONFIG_FILE}.diff
-
+TIME_TO_KILL=1
 
 . /etc/init.d/functions
 
 echo -n $PROGNAME -c ${CONFIG_FILE}
-./${PROGNAME} -c ${CONFIG_FILE} > ${PREPROC_RESULT} 2>&1 &  sleep 2; kill $!
+./${PROGNAME} -c ${CONFIG_FILE} > ${PREPROC_RESULT} 2> ${PREPROC_ERROR} &  sleep 1 ; kill $!
 grep timestamp ${PREPROC_RESULT} | cut -d',' -f2- > ${POSTPROC_RESULT}
 diff ${POSTPROC_RESULT} ${EXPECTED_RESULT} 2>&1 > ${TMP_DIFF}
 if [ $? -eq 0 ] 
 then
   echo_success
   echo
-  rm -f ${PREPROC_RESULT} ${POSTPROC_RESULT} ${TMP_DIFF}
+  rm -f ${PREPROC_RESULT} ${POSTPROC_RESULT} ${TMP_DIFF} ${PREPROC_ERROR}
 else
   echo_failure
   echo
