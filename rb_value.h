99e6f9c8a3e79f8c720fa7ea7d24c7d299041fe1
Eugenio Perez <eupm90@gmail.com>
2014-01-21 12:21:05 +0000

    Cosmetic changes

--- rb_value.h
+++ rb_value.h
@@ -14,14 +14,13 @@
 /// @note if you edit this structure, remember to edit monitor_value_copy
 struct monitor_value{
 	rd_memctx_t memctx;
+	rd_avl_node_t avl_node;
 
 	#ifdef MONITOR_VALUE_MAGIC
 	int magic; // Private data, don't need to use them outside.
 	#endif
 
-	time_t timestamp;
-	double value;
-	
+	/* config.json extracted */
 	bool sensor_id_valid;
 	int sensor_id;
 	const char * sensor_name;
@@ -29,17 +28,21 @@ struct monitor_value{
 	const char * send_name;       // Extern name. If not __gid__ nor __pos__, it is NULL and you have to check name.
 	                              // @todo make a function name() for do the last.
 	const char * instance_prefix;
-	unsigned int instance;
-	bool instance_valid;
-	const char * string_value;
-	/// Returns the way the value has been obtained.
-	const char * (*type)(void);
-	bool bad_value;
-	bool integer;
 	const char * unit;
 	const char * group_name;
 	const char * group_id;
-	rd_avl_node_t avl_node;
+	bool integer;
+
+	/* response */
+	time_t timestamp;
+	double value;
+	const char * string_value;
+	const char * (*type)(void); // way that the value has been obtained
+
+	/* vector response */
+	unsigned int instance;
+	bool instance_valid;
+	bool bad_value;
 };
 
 void monitor_value_copy(struct monitor_value *dst,const struct monitor_value *src);
