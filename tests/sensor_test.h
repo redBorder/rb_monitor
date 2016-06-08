ebf428e2a793f7361166cdba1956012ee6264984
Eugenio Pérez <eupm90@gmail.com>
2016-06-08 16:27:44 +0000

    Delete unused stuffs
    * _perthread_worker_info struct
    * sensor->sensor_id_valid

--- tests/sensor_test.h
+++ tests/sensor_test.h
@@ -8,17 +8,15 @@
 /// @TODO sepparate in .c file when we can extract struct definitions!
 static void test_sensor(const char *cjson_sensor, check_list_t *checks) {
 	struct _worker_info worker_info;
-	struct _perthread_worker_info pt_worker_info;
 	rd_lru_t *messages = rd_lru_new();
 
 	memset(&worker_info, 0, sizeof(worker_info));
-	memset(&pt_worker_info, 0, sizeof(pt_worker_info));
 
 	worker_info.monitor_values_tree = new_monitor_values_tree();
 	snmp_sess_init(&worker_info.default_session);
 	struct json_object *json_sensor = json_tokener_parse(cjson_sensor);
 
-	process_sensor(&worker_info, &pt_worker_info, json_sensor, messages);
+	process_sensor(&worker_info, json_sensor, messages);
 	json_list_check(checks, messages);
 	json_object_put(json_sensor);
 
