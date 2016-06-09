66ad3dd1987f20ab85c54b94d1708cd22eee6e51
Eugenio Pérez <eupm90@gmail.com>
2016-06-09 10:55:55 +0000

    Updated copyright

--- src/rb_snmp.h
+++ src/rb_snmp.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2015 Eneo Tecnologia S.L.
+  Copyright (C) 2016 Eneo Tecnologia S.L.
   Author: Eugenio Perez <eupm90@gmail.com>
 
   This program is free software: you can redistribute it and/or modify
